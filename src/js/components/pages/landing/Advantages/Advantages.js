977c4b9825f6465ea96f37fb9232ff8ec8322ac4
Demven <vinegar_dementor@mail.ru>
2016-05-12 19:09:45 +0300

    Advantages

--- src/js/components/pages/landing/Advantages/Advantages.js
+++ src/js/components/pages/landing/Advantages/Advantages.js
@@ -8,6 +8,30 @@ class Advantages {
   view() {
     return (
       <div className="Advantages">
+        <h2 className="Advantages__title">Преимущества использования приложения при чтении различных видов литературы</h2>
+        <ul className="Advantages__list">
+          <li className="Advantages__item">
+            <h3 className="Advantages__item-title">Художественная</h3>
+            <div className="Advantages__item-text">Помогает безошибочно выстроить сюжетную линию, наполняя ее впечатлениями от произведения.</div>
+          </li>
+          <li className="Advantages__item">
+            <h3 className="Advantages__item-title">Газеты и статьи</h3>
+            <div className="Advantages__item-text">Обучает моментально н пр аходить самые важные факты и запоминать их, а также читать вертикально, скользя по столбцу текста.</div>
+          </li>
+          <li className="Advantages__item">
+            <h3 className="Advantages__item-title">Учебная</h3>
+            <div className="Advantages__item-text">Прзволяет готовиться к экзамену за одну ночь.</div>
+          </li>
+          <li className="Advantages__item">
+            <h3 className="Advantages__item-title">Техническая</h3>
+            <div className="Advantages__item-text">Ускоряет понимание логических структур построения информации в сложных технических текстах.</div>
+          </li>
+          <li className="Advantages__item">
+            <h3 className="Advantages__item-title">Научно-популярная</h3>
+            <div className="Advantages__item-text">Заставляет  выявлять ключевые тезисы и подкреплять их фактами из текста.</div>
+          </li>
+          <li className="Advantages__item"></li>
+        </ul>
       </div>
     );
   }
