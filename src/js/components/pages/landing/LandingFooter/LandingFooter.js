dd6cf334242af6e79a1c7e803276f3d64c6d5a5d
Demven <vinegar_dementor@mail.ru>
2016-05-13 20:20:33 +0300

    Footer

--- src/js/components/pages/landing/LandingFooter/LandingFooter.js
+++ src/js/components/pages/landing/LandingFooter/LandingFooter.js
@@ -2,12 +2,14 @@ import m from 'mithril';
 
 class Footer {
   constructor() {
-    this.name = m.prop('Footer');
+    this.name = m.prop('LandingFooter');
   }
 
   view() {
     return (
-      <div className="Footer">
+      <div className="LandingFooter">
+        <img src="/images/ic/logo-shadow.png" className="LandingFooter__logo" alt="Enlite logotype"/>
+        <div className="LandingFooter__feedback">Обратная связь</div>
       </div>
     );
   }
