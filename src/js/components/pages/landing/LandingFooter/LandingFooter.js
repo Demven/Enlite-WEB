a6f203646ca2bda0efaca4c443d2062596479390
Demven <vinegar_dementor@mail.ru>
2016-07-09 00:37:42 +0300

    Link for feedback;

--- src/js/components/pages/landing/LandingFooter/LandingFooter.js
+++ src/js/components/pages/landing/LandingFooter/LandingFooter.js
@@ -8,8 +8,14 @@ class Footer {
   view() {
     return (
       <section className="LandingFooter">
-        <img src="/images/ic/logo-shadow.png" className="LandingFooter__logo" alt="Enlite logotype"/>
-        <div className="LandingFooter__feedback">Обратная связь</div>
+        <img src="/images/ic/logo-shadow.png" className="LandingFooter__logo" alt="Enlite logotype" />
+        <a
+          href="mailto:enlitedmind@gmail.com"
+          title="enlitedmind@gmail.com"
+          className="LandingFooter__feedback-link"
+        >
+          <div className="LandingFooter__feedback">Обратная связь</div>
+        </a>
       </section>
     );
   }
